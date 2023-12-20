88e274236a1aed31aa2369605f4f15533aa6a06b
Bloodybag1337 <legostinov.artyom@gmail.com>
2023-12-20 23:05:45 +0300

    Initial commit

---
+++ Домашнее задание MakeYouPro - 15/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Разработайте заготовку проекта интернет-магазина техники,
+который продаёт смартфоны, планшеты и ноутбуки.
+
+Все товары должны содержать в себе:
+-название,
+-производителя,
+-количество памяти,
+-диагональ экрана,
+-цену.
+
+При этом смартфоны и планшеты содержат информацию о разрешении камеры,
+планшеты - информацию о поддержке стилуса,
+ноутбуки - информацию о времени работы от аккумулятора.
+
+Все товары содержат 2 метода:
+-Получение короткой информации - который возвращает производителя, название и цену в текстовом виде;
+-Получение цены, который получает на вход количество и возвращает цену с учетом оптовой скидки
+(опт начинается с 5 и более позиций):
+смартфоны - 7 %,
+планшеты - 9 %,
+ноутбуки - 5 %.
+
+Все товары из одной категории можно сравнить между собой через метод Equals
+Необходимо создать 4 класса - родительский товар и 3 класса товара отдельно.
+*/
+
+using Inheritance;
+
+Smartphones[] smartphone = new Smartphones[]
+{
+new Smartphones("iPhone", "Apple", 100000),
+new Smartphones("Samsung", "Android", 85000),
+new Smartphones("Xiaomi", "Android", 32000)
+};
+
+Tablets[] tablet = new Tablets[]
+{
+new Tablets("iPad", "Apple", 150000),
+new Tablets("iPad", "Apple", 150000)
+};
+
+Notebooks[] notebook = new Notebooks[]
+{
+new Notebooks("Macbook", "Apple", 250000),
+new Notebooks("Asus", "Asus", 75000),
+new Notebooks("Dell", "Dell", 55000),
+new Notebooks("HP", "HP", 80000)
+};
+
+for (int i = 0; i < smartphone.Length; i++)
+{
+    smartphone[i].GiveMeShortInfo();
+    smartphone[i].GetAPrice();
+    Console.WriteLine();
+}
+
+for (int i = 0; i < tablet.Length; i++)
+{
+    tablet[i].GiveMeShortInfo();
+    tablet[i].GetAPrice();
+    Console.WriteLine();
+}
+
+if (tablet[0].Equals(tablet[1]))
+    Console.WriteLine("Элементы равны\n");
+else
+    Console.WriteLine("Элементы не равны\n");
+
+for (int i = 0; i < notebook.Length; i++)
+{
+    notebook[i].GiveMeShortInfo();
+    notebook[i].GetAPrice();
+    Console.WriteLine();
+}
+
+Console.ReadKey();
---
+++ Домашнее задание MakeYouPro - 15/Technique.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Inheritance
+{
+	public class Technique //родительский класс
+	{
+		public string Name { get; set; }
+		public string Manufacturer { get; set; }
+		public int MemoryCapacity { get; set; }
+		public int ScreenDiagonal { get; set; }
+		public double Price { get; set; }
+
+		public Technique (string name, string manufacturer, double price)
+		{
+			Name = name;
+			Manufacturer = manufacturer;
+			Price = price;
+		}
+
+		public virtual void GiveMeShortInfo()
+        //virtual - потому что в наследниках вместо слова "устройство" будет его название
+		//(смартфон, ноутбук или планшет)
+        {
+            Console.WriteLine($"Устройство {Name} от бренда {Manufacturer}. Стоимость: {Price} рублей");
+		}
+
+		public void GetAPrice()
+		{
+			Console.WriteLine("Сколько штук Вы хотите купить?");
+			double n = Convert.ToDouble(Console.ReadLine());
+			if (n >= 5)
+			{
+				Price = n * Price - Price * (n/100);
+				Console.WriteLine($"Общая стоимость составит {Price} рублей");
+			}
+			else
+				Console.WriteLine($"Общая стоимость составит {n * Price} рублей");
+		}
+	}
+
+	public class Smartphones : Technique //класс-наследник
+    {
+		public int CameraResolution { get; set; }
+
+		public Smartphones (string name, string manufacturer, double price) :
+			base (name, manufacturer, price) //наследование конструктора от базового класса
+		{
+
+		}
+
+        public override void GiveMeShortInfo()
+        {
+            Console.WriteLine($"Смартфон {Name} от бренда {Manufacturer}. Стоимость: {Price} рублей");
+        }
+    }
+
+	public class Tablets : Technique //класс-наследник
+    {
+		public int CameraResolution { get; set; }
+		public bool StylusSupport { get; set; }
+
+        public Tablets (string name, string manufacturer, double price) :
+			base(name, manufacturer, price) //наследование конструктора от базового класса
+        {
+
+        }
+
+        public override void GiveMeShortInfo()
+        {
+            Console.WriteLine($"Планшет {Name} от бренда {Manufacturer}. Стоимость: {Price} рублей");
+        }
+
+        public override bool Equals(object? obj)
+			//проверка метода Equals на примере планшетов с одинаковыми данными
+        {
+			return obj is Tablets tablet &&
+				Name == tablet.Name &&
+                Manufacturer == tablet.Manufacturer &&
+                Price == tablet.Price;
+        }
+    }
+
+	public class Notebooks : Technique //класс-наследник
+    {
+        public int BatteryLife { get; set; }
+
+        public Notebooks (string name, string manufacturer, double price) :
+			base(name, manufacturer, price) //наследование конструктора от базового класса
+        {
+
+        }
+
+        public override void GiveMeShortInfo()
+        {
+            Console.WriteLine($"Ноутбук {Name} от бренда {Manufacturer}. Стоимость: {Price} рублей");
+        }
+    }
+}
